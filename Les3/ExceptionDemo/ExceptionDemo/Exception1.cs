982cb13553d116eae9217b291dfca726da8ace80
Luc Vervoort <luc.vervoort@hogent.be>
2021-02-19 09:54:14 +0100

    Exceptions

---
+++ Les3/ExceptionDemo/ExceptionDemo/Exception1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ExceptionDemo
+{
+    public class MyException: Exception
+    {
+        public override string ToString()
+        {
+            var extraInfo = "Mijn exception: ";
+            return extraInfo + base.ToString();
+        }
+    }
+
+    public class Exception1
+    {
+        public int X { get; set; }
+
+        public void ExecuteRead()
+        {
+            try
+            {
+                string input = Console.ReadLine();
+                int converted = Convert.ToInt32(input);
+            }
+            catch(FormatException ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Je moet een integer ingeven: " + ex.ToString());
+            }            
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine("Verkeerde invoer: " + e.Message);
+            }                    
+        }
+
+        public void CallMe()
+        {
+            try
+            {
+                try
+                {
+                    //throw new NotImplementedException();
+                    throw new Exception("Dit is mijn eigen uitzondering - ik moet dit nog implementeren");
+                }
+                catch (Exception e)
+                {
+                    throw new MyException();
+                }
+                System.Diagnostics.Debug.WriteLine("En we blijven doorgaan...");
+            }
+            catch(MyException e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.ToString());
+            }
+        }
+    }
+}
---
+++ Les3/ExceptionDemo/ExceptionDemoApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using ExceptionDemo;
+using System;
+
+namespace ExceptionDemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 5; // POD
+            int j = i + 1;
+
+            Exception1 myObject = null; // = new Exception1();
+            try
+            {
+                myObject.X = 5;
+            }
+            catch(Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine("Ik heb een fout voor: " + e.Message);
+                //throw; // alsof er geen catch stond!
+                // throw e; // is ook goed, maar niet aangeraden als je enkel de exception terug doorgeeft
+            }
+            System.Diagnostics.Debug.WriteLine("We gaan toch door met onze applicatie dankzij de catch");
+
+            myObject = new Exception1();
+            myObject.ExecuteRead();
+            myObject.CallMe();
+        }
+    }
+}
