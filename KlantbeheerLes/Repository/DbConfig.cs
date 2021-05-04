7e5e94a917d03ff672ddd003a23c5660c7c66dea
Luc Vervoort <luc.vervoort@hogent.be>
2021-05-04 09:14:13 +0200

    WPF

--- KlantbeheerLes/Repository/DbConfig.cs
+++ KlantbeheerLes/Repository/DbConfig.cs
@@ -2,16 +2,21 @@
 
 namespace Repository
 {
-    public static class DbConfig
+    public static class DbConfig // static: nooit een instantie; dwz geen ctor
     {
         #region Properties
         // Een eenvoudig singleton... "DESIGN PATTERN" SOLID
         private static SqlConnection _sqlConnection = null;
-        public static SqlConnection Connection { get { if (_sqlConnection == null) { _sqlConnection = CreateConnection(); } return _sqlConnection; } }
+
+        // Instantie van SqlConnection wordt LAZY gemaakt: op allerlaatste moment aanmaken
+        // "Een" keer aangemaakt => "single-"ton
+        public static SqlConnection Connection { get { if (_sqlConnection == null) { _sqlConnection = CreateConnection(); } return _sqlConnection; } /* geen set: zo controleren we toegang tot de instantie */ }
         #endregion
 
+        // Indien ik toch een aparte extra connectie wil, gebruik ik de volgende public method:
         public static SqlConnection CreateConnection()
         {
+            // LVET TODO: later zullen we connection string nog verplaatsen naar een configuratiebestand
             return new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=StoreDb;Integrated Security=True");
         }
     }
